61db4cab12f7ad2543282f8444abd3a5d888f8f0
Rodrigo Barbosa Anselmo <rodrigo@MacBook-Pro-de-Rodrigo.local>
2023-09-05 12:32:33 -0300

    feat: Add auth to app module with public route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
+  imports: [
+    ThrottlerModule.forRoot({ throttlers: [{ limit: 10, ttl: 60 }] }),
+    PrismaModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
