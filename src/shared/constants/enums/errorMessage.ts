344085f2f30245cc343586f7bc8fa10951f501ec
Rodrigo Barbosa Anselmo <rodrigo@MacBook-Pro-de-Rodrigo.local>
2023-09-07 10:52:25 -0300

    feat: errors message

--- src/shared/constants/enums/errorMessage.ts
+++ src/shared/constants/enums/errorMessage.ts
@@ -1,3 +1,6 @@
-export enum ErrorAuthEnum {
+export enum ErrorEnum {
   GOOGLE_USER_ERROR = 'Erro ao logar com conta Google',
+  FIND_USER_ERROR = 'Erro ao encontrar usuário',
+  EXCHANGE_EXISTS = 'Troca já existe',
+  BOOK_NOT_FOUND = 'Livro não encontrado',
 }
