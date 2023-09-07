7e37ba530b6306874af7351ad78b4d2e4bd17173
Rodrigo Barbosa Anselmo <rodrigo@MacBook-Pro-de-Rodrigo.local>
2023-09-07 10:51:01 -0300

    feat: add auth module google use case

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,25 +1,39 @@
 import { Module } from '@nestjs/common';
 
-import { FirebaseProvider } from '../../shared/providers/FirebaseProvider/implementations/FirebaseProvider';
+import { FirebaseProvider } from '../../shared/providers/JwtProvider/implementations/FirebaseProvider';
 import { AuthController } from './controller/session/auth.controller';
 import { GoogleLoginUseCase } from './useCases/google-login';
 import { UsersRepository } from './repositories/implementations/UsersRepository';
 import { IUsersRepository } from './repositories/models/IUsersRepository.types';
-import { IFirebaseProvider } from '../../shared/providers/FirebaseProvider/models/IFirebaseProvider.types';
+import { IJwtProvider } from '../../shared/providers/JwtProvider/models/IJwtProvider.types';
+import { FindUserByIdUseCase } from './useCases/find-user-by-id';
 
 const GoogleLoginUseCaseProvider = {
   provide: GoogleLoginUseCase,
   useFactory: (
     UsersRepository: IUsersRepository,
-    firebaseProvider: IFirebaseProvider,
+    jwtProvider: IJwtProvider,
   ) => {
-    return new GoogleLoginUseCase(UsersRepository, firebaseProvider);
+    return new GoogleLoginUseCase(UsersRepository, jwtProvider);
   },
-  inject: [FirebaseProvider, UsersRepository],
+  inject: [UsersRepository, FirebaseProvider],
+};
+
+const FindUserByGoogleIdUseCaseProvider = {
+  provide: FindUserByIdUseCase,
+  useFactory: (UsersRepository: IUsersRepository) => {
+    return new FindUserByIdUseCase(UsersRepository);
+  },
+  inject: [UsersRepository],
 };
 
 @Module({
   controllers: [AuthController],
-  providers: [FirebaseProvider, UsersRepository, GoogleLoginUseCaseProvider],
+  providers: [
+    FirebaseProvider,
+    UsersRepository,
+    FindUserByGoogleIdUseCaseProvider,
+    GoogleLoginUseCaseProvider,
+  ],
 })
 export class AuthModule {}
--- src/modules/auth/controller/session/auth.controller.ts
+++ src/modules/auth/controller/session/auth.controller.ts
@@ -1,14 +1,27 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { UserPayloadDto } from './../../../../shared/dto/user-payload.dto';
 
 import { Public } from '../../../../shared/decorators/public.decorator';
 import { LoginGoogleUserDto } from '../../dto/login-user.dto';
 import { GoogleLoginUseCase } from '../../useCases/google-login/google-login';
+import { User } from '../../../../shared/decorators/user.decorator';
+import { FindUserByIdUseCase } from '../../useCases/find-user-by-id';
 
 @ApiTags('Authentication')
-@Controller()
+@Controller('auth')
 export class AuthController {
-  constructor(private readonly googleLoginUseCase: GoogleLoginUseCase) {}
+  constructor(
+    private readonly googleLoginUseCase: GoogleLoginUseCase,
+    private readonly findUserByGoogleIdUseCase: FindUserByIdUseCase,
+  ) {}
+
+  @Get('me')
+  async findMe(@User() userPayloadDto: UserPayloadDto) {
+    return this.findUserByGoogleIdUseCase.execute({
+      id: userPayloadDto.id,
+    });
+  }
 
   @Public()
   @Post('session/google')
