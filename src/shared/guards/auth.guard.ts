46e7482d78d7b045ca358384022c8940c381687f
Rodrigo Barbosa Anselmo <rodrigo@MacBook-Pro-de-Rodrigo.local>
2023-09-07 10:50:15 -0300

    feat: create auth guard for firebase

---
+++ src/shared/guards/auth.guard.ts
@@ -0,0 +1,52 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Request } from 'express';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
+import { UserPayloadDto } from '../dto/user-payload.dto';
+import { IJwtProvider } from '../providers/JwtProvider/models/IJwtProvider.types';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private jwtProvider: IJwtProvider,
+    private reflector: Reflector,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const payload = await this.jwtProvider.validateToken(token);
+
+      request['user'] = {
+        email: payload.email,
+        id: payload.uid,
+      } satisfies UserPayloadDto;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
