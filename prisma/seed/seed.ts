c350b5dbd27c795445d74385bec51c509fc39fc7
Rodrigo Barbosa Anselmo <rodrigo@MacBook-Pro-de-Rodrigo.local>
2023-09-07 16:49:40 -0300

    feat: update seed with more complex data

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -10,9 +10,19 @@ async function main() {
   const books = readFileSync('prisma/seed/json/books.json', 'utf8');
   const booksJson = JSON.parse(books) as GoogleBooksResponseDto;
 
+  const user = await prisma.user.create({
+    data: {
+      id: 'y3qfSuVPXxvrEqnBjAs33',
+      email: 'rodrigobanselmo@gmail.com',
+      name: 'Rodrigo Banselmo',
+      photoUrl:
+        'https://lh3.googleusercontent.com/a-/AFdZucouIGn-Xy2T2QndjbIZxKUpTGrDIJL0gbBnsnPX=s96-c',
+    },
+  });
+
   await Promise.all(
     booksJson.items.map(async (book) => {
-      await prisma.book.create({
+      const bookCreated = await prisma.book.create({
         data: {
           title: book.volumeInfo.title,
           authors: book.volumeInfo.authors,
@@ -22,6 +32,13 @@ async function main() {
           language: book.volumeInfo.language,
           smallThumbnail: book.volumeInfo.imageLinks?.smallThumbnail,
           thumbnail: book.volumeInfo.imageLinks?.thumbnail,
+        },
+      });
+
+      await prisma.usersBooks.create({
+        data: {
+          bookId: bookCreated.id,
+          userId: user.id,
         },
       });
     }),
